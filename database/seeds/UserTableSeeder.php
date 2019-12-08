ecc7a1f396137dce1df35539693382e59c6a2f6c
Administrator <888222y@sina.com>
2019-12-08 21:49:48 +0800

    laravel练习web服务端

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = bcrypt('secret');
+        $users = factory(User::class)->times(49)->make()->each(function ($user, $i) use ($password)  {
+            if ($i == 0) {
+                $user->name = 'admin';
+                $user->email = 'admin@estgroupe.com';
+                $user->github_name = 'admin';
+                $user->verified = 1;
+            }
+            $user->password = $password;
+            $user->github_id = $i + 1;
+        });
+        User::insert($users->toArray());
+        $hall_of_fame = Role::addRole('HallOfFame', '名人堂');
+        $users = User::all();
+        foreach ($users as $key => $user) {
+            $user->attachRole($hall_of_fame);
+        }
+    }
+}
